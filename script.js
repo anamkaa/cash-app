d65b9195e5f8498794a98300f25f3cc46e24d65b
anamkaa <anamikakashyap99@gmail.com>
2021-08-01 23:36:21 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var cashAmount = document.querySelector("#cash-amount");
+var cashGiven = document.querySelector("#cash-given");
+var btnCheck = document.querySelector("#btn-check");
+
+btnCheck.addEventListener("click",printText);
+
+function printText(){
+    console.log("button clicked");
+}
