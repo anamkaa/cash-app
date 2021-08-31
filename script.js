a52c003f41c37d08f22bdd8de6e6f6d977f1dd82
anamkaa <anamikakashyap99@gmail.com>
2021-08-31 16:44:25 +0530

    fixed minor changes and added new style

--- script.js
+++ script.js
@@ -6,70 +6,67 @@ const firstMessage = document.querySelector("#first-message");
 const btnCheck = document.querySelector("#btn-check");
 const showMessage = document.querySelector("#show-message");
 const noOfnote = document.querySelectorAll(".no-of-note");
+const rA = document.querySelector("#ra");
 
-firstFunction();
+// firstFunction();
 
-const availableNotes = [2000,500,100,20,10,5,1];
-
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 btnNxt.addEventListener("click", showNextSection);
-
 btnCheck.addEventListener("click", validateCash);
 
-function firstFunction(){
-    nextSec.style.display = "none";
+// function firstFunction() {
+//   nextSec.style.display = "none";
+// }
+
+function showNextSection() {
+    // nextSec.style.display = "flex";
+//   btnNxt.style.display = "none";
+  firstMessage.style.display = "none";
+
+
+  if (billAmount.value > 0) {
+    nextSec.style.display = "flex";
+    btnNxt.style.display = "none";
+  } else {
+    firstMessage.style.display = "block";
+    firstMessage.innerText = "Enter valid bill amount";
+  }
 }
 
-function showNextSection(){
+function validateCash() {
+  showMessage.style.display = "none";
 
-    firstMessage.style.display = "none";
+  console.log("billAmount: " + billAmount.value + ", cashGiven: " + cashGiven.value);
 
-    if(billAmount.value>0){
-        nextSec.style.display = "block";
-        btnNxt.style.display = "none";
+  if (billAmount.value > 0 && cashGiven.value > 0) {
+    if (
+        parseInt(cashGiven.value) > parseInt(billAmount.value) ||
+        parseInt(cashGiven.value) === parseInt(billAmount.value)
+    ) {
+    const netCash = Number(parseInt(cashGiven.value) - parseInt(billAmount.value));
+    console.log(netCash);
+      calculateCash(netCash);
+      
+    } else {
+      showMessage.style.display = "block";
+      showMessage.innerText = "Insufficient Amount Paid.";
     }
-
-    else{
-      firstMessage.style.display = "block";
-      firstMessage.innerText = "Enter valid bill amount";
-    }
-    
+  } else {
+    showMessage.style.display = "block";
+    showMessage.innerText = "Invalid Cash Amount";
+  }
 }
 
-function validateCash(){
-    showMessage.style.display = "none";
+function calculateCash(netCash) {
+    rA.innerText = netCash;
 
-    if(billAmount.value>0 && cashGiven.value>0){
-        
-        const netCash = Number(cashGiven.value - billAmount.value);
+  // traverse through all the available notes
+  for (let i = 0; i < availableNotes.length; i++) {
+    // calculating number of note of each denomination
+    const numberOfNotes = Math.floor(netCash / availableNotes[i]);
 
-        if(cashGiven.value>billAmount.value || cashGiven.value===billAmount.value ) {
-           
-         calculateCash(netCash);
-        }
+    netCash = netCash - numberOfNotes * availableNotes[i];
 
-        else{
-            showMessage.style.display ="block";
-            showMessage.innerText = "Insufficient Amount Paid."
-        }
-    }
-
-    else{
-        showMessage.style.display = "block";
-        showMessage.innerText = "Invalid Cash Amount";
-    }
-
-}
-
-function calculateCash(netCash){
-
-    // traverse through all the available notes
-    for(let i=0; i<availableNotes.length; i++){
-
-        // calculating number of note of each denomination
-        const numberOfNotes = Math.floor(netCash/availableNotes[i]);
-
-         netCash = netCash - numberOfNotes * availableNotes[i];
-
-        noOfnote[i].innerText = numberOfNotes;
-    }
+    noOfnote[i].innerText = numberOfNotes;
+  }
 }
