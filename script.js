456f61197b6a3a5af9229b27f86cdfea32b130d2
anamkaa <anamikakashyap99@gmail.com>
2021-08-31 12:07:44 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
+const nextSec = document.querySelector("#next-sec");
+const btnNxt = document.querySelector("#nxt-btn");
+const firstMessage = document.querySelector("#first-message");
 const btnCheck = document.querySelector("#btn-check");
 const showMessage = document.querySelector("#show-message");
 const noOfnote = document.querySelectorAll(".no-of-note");
 
+firstFunction();
+
 const availableNotes = [2000,500,100,20,10,5,1];
 
+btnNxt.addEventListener("click", showNextSection);
+
 btnCheck.addEventListener("click", validateCash);
+
+function firstFunction(){
+    nextSec.style.display = "none";
+}
+
+function showNextSection(){
+
+    firstMessage.style.display = "none";
+
+    if(billAmount.value>0){
+        nextSec.style.display = "block";
+        btnNxt.style.display = "none";
+    }
+
+    else{
+      firstMessage.style.display = "block";
+      firstMessage.innerText = "Enter valid bill amount";
+    }
+    
+}
 
 function validateCash(){
     showMessage.style.display = "none";
 
-    if(billAmount.value>0){
+    if(billAmount.value>0 && cashGiven.value>0){
         
-        if(cashGiven.value>billAmount.value || cashGiven.value===billAmount.value){
+        const netCash = Number(cashGiven.value - billAmount.value);
+
+        if(cashGiven.value>billAmount.value || cashGiven.value===billAmount.value ) {
            
-        //    net cash to be returned is calculated 
-            const netCash = (cashGiven.value - billAmount.value);
-           calculateCash(netCash);
+         calculateCash(netCash);
         }
 
         else{
@@ -28,7 +55,7 @@ function validateCash(){
 
     else{
         showMessage.style.display = "block";
-        showMessage.innerText = "Invalid Bill Amount";
+        showMessage.innerText = "Invalid Cash Amount";
     }
 
 }
@@ -39,9 +66,9 @@ function calculateCash(netCash){
     for(let i=0; i<availableNotes.length; i++){
 
         // calculating number of note of each denomination
-        const numberOfNotes = Math.trunc(netCash/availableNotes[i]);
+        const numberOfNotes = Math.floor(netCash/availableNotes[i]);
 
-         netCash = netCash%availableNotes[i];
+         netCash = netCash - numberOfNotes * availableNotes[i];
 
         noOfnote[i].innerText = numberOfNotes;
     }
